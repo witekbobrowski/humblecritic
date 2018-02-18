955d3b2e357efbcbe16cbaeb6759d0e505b24b16
witekbobrowski <witek@bobrowski.com.pl>
2018-02-18 12:32:02 +0100

    Add test for search_books from goodreads client.

--- humble_rating/goodreads/client.py
+++ humble_rating/goodreads/client.py
@@ -4,7 +4,15 @@
 
 import requests
 import xmltodict
+from enum import Enum
+from .book import Book
 
+class HTTPMethod(Enum):
+    POST = "POST"
+    GET = "GET"
+    PUT = "PUT"
+    PATCH = "PATCH"
+    DELETE = "DELETE"
 
 class GoodreadsClient:
 
@@ -15,16 +23,22 @@ class GoodreadsClient:
 
     def request(self, method, api, params):
         base_url = "http://www.goodreads.com"
-        return xmltodict.parse(self.session.request(method, base_url + api, params=params).content)["GoodreadsResponse"]
+        response = self.session.request(method, base_url + api, params=params)
+        return xmltodict.parse(response.content)["GoodreadsResponse"]
 
     def search_book(self, book):
-        method = "GET"
+        method = HTTPMethod.GET.value
         api_path = "/search/index.xml"
         params = {"q": book, "key": self.key}
-        return self.request(method, api_path, params)
+        response = self.request(method, api_path, params)
+        if type(response["search"]["results"]["work"]) == list:
+            return [results["best_book"] for results in response["search"]["results"]["work"]]
+        else:
+            return [response["search"]["results"]["work"]]
 
-    def show_book(self, id, format):
-        method = "GET"
-        api_path = "/book/show." + str(format)
-        params = {"format": format, "id": id, "key": self.key}
-        return self.request(method, api_path, params)
+    def show_book(self, id):
+        method = HTTPMethod.GET.value
+        api_path = "/book/show.xml"
+        params = {"format": "xml", "id": id, "key": self.key}
+        response = self.request(method, api_path, params)
+        return Book(response["book"])
--- tests/goodreads_test.py
+++ tests/goodreads_test.py
@@ -5,12 +5,19 @@ import unittest
 
 class TestGoodreadsClientSuite(unittest.TestCase):
 
+    client = hr.GoodreadsClient("dev-key", "secret")
+
     def test_show_book(self):
         book_id = 1
-        gc = hr.GoodreadsClient("dev-key", "secret")
-        book = hr.Book(gc.show_book(book_id, "xml")["book"])
+        book = self.client.show_book(book_id)
         assert book.title == "Harry Potter and the Half-Blood Prince (Harry Potter, #6)"
 
+    def test_search_books(self):
+        book_title = "Advanced Swift"
+        books = self.client.search_book(book_title)
+        if len(books) == 0:
+            assert False
+        assert books[0]["title"] == book_title
 
 if __name__ == '__main__':
     unittest.main()
